8098476ff728c0ea68934d1368eb36410046a7af
Karly Mareka <karly.mareka@gmail.com>
2018-08-22 18:47:20 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,5 +23,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.scan(/\A\(*[0-9]{3}\)*\-*\ *[0-9]{3}\-*[0-9]{4}$/) 
+  return phone.scan(/\A\(*[0-9]{3}\)*\-*\ *[0-9]{3}\-*[0-9]{4}$/) 
 end
